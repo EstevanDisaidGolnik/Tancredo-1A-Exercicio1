c0d45059929660c570b63cbb743bf100f1549971
Estevan Neri Disaid Emerenciano <141413662+EstevanDisaidGolnik@users.noreply.github.com>
2023-08-11 08:18:20 -0300

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,19 @@
+function setup() {
+  createCanvas(800,800);
+  background("blue");
+
+
+}
+
+function draw() {
+  
+  
+  
+  stroke("silver");
+ 
+  fill("red");
+
+  if(mouseIsPressed){rect(mouseX,mouseY,35,35);}
+
+
+}
